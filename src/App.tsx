0022e3832fca52e351c80cc23c93703e0fbf76f7
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-05-30 15:19:06 +0530

    main:4th commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import BaseComponent from './BaseComponent/BaseComponent';
 
 function App() {
   return (
    <div>
-    
+    <BaseComponent title='Frontend Developer' name='Sanket Washimkar'  /> 
    </div>
   );
 }
