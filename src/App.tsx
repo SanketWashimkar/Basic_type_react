539fd4ea0123c486a5ab7092c4f6dbe34c04b0f6
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-05-31 21:07:57 +0530

    main:6th commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import BaseComponent from './BaseComponent/BaseComponent';
+import UserMangment from './UserMangment/UserMangment';
 
 function App() {
   return (
    <div>
-    <BaseComponent title='Frontend Developer' name='Sanket Washimkar'  /> 
+ 
+    {/* <BaseComponent title='Frontend Developer' name='Sanket Washimkar'  />  */}
+    <UserMangment />
    </div>
   );
 }
