8a73e5e6db8aa6cecbd037c70bfc2b6417641fde
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-05-31 21:08:30 +0530

    main:8th commit

---
+++ src/UserMangment/UserMangment.tsx
@@ -0,0 +1,16 @@
+import styless from './UserMangment.module.scss';
+
+const UserMangment = () => {
+
+return (
+ <div className={styless.mainContainer}>
+<p> UserMangment</p>
+ </div>
+
+)
+
+
+
+}
+
+export default UserMangment;
